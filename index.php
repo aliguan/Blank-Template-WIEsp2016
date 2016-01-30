17fdff929ad511e1c1c552c2d460ffe598b6f55b
Alice Guan <aliguan726@gmail.com>
2016-01-30 17:02:16 -0500

    Add Skeleton CSS

--- index.php
+++ index.php
@@ -3,11 +3,15 @@
 <head>
     <meta charset="utf-8">
     <title>Web Interactvitiy and Engagement</title>
+
+    <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
     <h1>Web Interactivity and Engagement</h1>
     <h2>Spring 2016</h2>
 
     <p>This is where the blogs will go.</p>
+
+    <p><?php echo "This will parse on the server, but display on the browser." ?> </p>
 </body>
 </html>
